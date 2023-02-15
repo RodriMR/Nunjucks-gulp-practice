c3765bc6a29fb60d594d2ace05d4476c4dfad454
RodriMR <rodrimr2008@gmail.com>
2023-02-14 22:26:42 -0300

    Gulp setup watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 const gulp = require("gulp");
+const nunjucksRender = require("gulp-nunjucks-render");
+const browserSync = require("browser-sync").create();
 
-gulp.task("default", function (done) {
-  console.log("this is the default taks");
-  done();
+gulp.task("html", function () {
+  return gulp
+    .src("./views/*.njk")
+    .pipe(nunjucksRender({ path: ["./views"] }))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("watch", function () {
+  browserSync.init({
+    server: { baseDir: "./" },
+  });
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./views/**/*.njk", gulp.series("html"));
 });
